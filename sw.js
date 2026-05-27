94dd59484d163e1d520d165b443b3365cb3b0653
andrew-ahern <a.ahern.x@gmail.com>
2026-05-27 16:33:24 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 /* FitLog Service Worker
    Caches the app shell so it works offline after first load. */
 
-const CACHE_NAME = 'fitlog-v6';
+const CACHE_NAME = 'fitlog-v7';
 
 const SHELL = [
   './index.html',
